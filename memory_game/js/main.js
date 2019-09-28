ac1161bd0ee45cc43faf8ffab299df226afd6946
Fahad Al-Qurashi <Home@MacBook-Pros-MacBook-Pro.local>
2019-09-28 17:04:38 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,17 @@
-console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = "User flipped" + " " + cards[0];
+var cardTwo = "User flipped" + " " + cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		} else {
+		alert("Sorry, try again!");
+	}
+	}
+
